b38e3dda34f930be5e7bd41b6d95d2355be20ff2
Ehsan Azimi <ehsan.azimi@kaco-newenergy.de>
2023-06-14 13:42:15 +0200

    write writer.c

---
+++ finder-app/writer.c
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    if(argc != 3)
+        return 1;
+    
+    char *writefile = argv[1];
+    char *writestr = argv[2];
+
+    char *writefile_cpy = (char*) malloc(strlen(writefile+1) * sizeof(char));
+    strcpy(writefile_cpy, writefile);
+
+    char *dirpath = dirname(writefile_cpy);
+    free(writefile_cpy);
+
+    struct stat st = {0};
+    if(stat(dirpath, &st) == -1)
+        mkdir(dirpath, 0777);
+    
+    int pfd = open(writefile, O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if(pfd == -1)
+    {
+        printf("file could not be created\n");
+        return 1;
+    }
+
+    int wrc = write(pfd, writestr, strlen(writestr));
+    close(pfd);
+
+    return 0;
+}
