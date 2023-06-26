23ad70d6d120a8d52301e3a0295086d02a54d614
Ehsan Azimi <ehsan.azimi@kaco-newenergy.de>
2023-06-26 10:40:25 +0200

    copy autorun-qemu.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -120,7 +120,8 @@ sudo cp writer ${ROOTFS}/home
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-sudo cp finder.sh finder-test.sh ../conf/username.txt ${ROOTFS}/home   
+sudo cp finder.sh finder-test.sh ../conf/username.txt ${ROOTFS}/home 
+sudo cp autorun-qemu.sh ${ROOTFS}/home   
 
 # TODO: Chown the root directory
 
