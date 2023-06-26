24a577fdfb6b3e43cb4b0f094825b18120d34521
Ehsan Azimi <ehsan.azimi@kaco-newenergy.de>
2023-06-26 09:56:29 +0200

    add library dependencies to rootf

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,10 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+INTRPRTR="/var/lib/docker/overlay2/e1b53b70d0542d3203c44d2175bb8e367a0c6f304ecfef506b0f183092f72ca8/diff/usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1"
+LIBRESOLV="/var/lib/docker/overlay2/acb8accd7a02a0531c8bb2cf363509d577334b7fc1627315e71ffc3c7b74e8dc/diff/usr/lib/x86_64-linux-gnu/libresolv.so.2"
+LIBC="/var/lib/docker/overlay2/acb8accd7a02a0531c8bb2cf363509d577334b7fc1627315e71ffc3c7b74e8dc/diff/usr/lib/x86_64-linux-gnu/libc.so.6"
+LIBM="/var/lib/docker/overlay2/acb8accd7a02a0531c8bb2cf363509d577334b7fc1627315e71ffc3c7b74e8dc/diff/usr/lib/x86_64-linux-gnu/libm.so.6"
 
 if [ $# -lt 1 ]
 then
@@ -55,32 +59,52 @@ fi
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
-fi
-
-# TODO: Create necessary base directories
-
-# cd "$OUTDIR"
-# if [ ! -d "${OUTDIR}/busybox" ]
+# if [ -d "${OUTDIR}/rootfs" ]
 # then
-# git clone git://busybox.net/busybox.git
-#     cd busybox
-#     git checkout ${BUSYBOX_VERSION}
-#     # TODO:  Configure busybox
-# else
-#     cd busybox
+# 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+#     sudo rm  -rf ${OUTDIR}/rootfs
 # fi
 
-# # TODO: Make and install busybox
+# TODO: Create necessary base directories
+ROOTFS=${OUTDIR}/rootfs
+mkdir -p ${ROOTFS}
+cd ${ROOTFS}
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr/bin usr/lib usr/sbin var/log
 
-# echo "Library dependencies"
-# ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-# ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/busybox" ]; then
+git clone git://busybox.net/busybox.git
+    cd busybox
+    git checkout ${BUSYBOX_VERSION}
+    # TODO:  Configure busybox
+    make distclean
+    make defconfig
+else
+    cd busybox
+fi
+
+# TODO: Make and install busybox
+# make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+# make CONFIG_PREFIX=${ROOTFS} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+PROG_INTERPRETER=$(${CROSS_COMPILE}readelf -a ${ROOTFS}/bin/busybox | grep "program interpreter")
+PROG_INTERPRETER=$(echo ${PROG_INTERPRETER} | sed 's/.*\/\(.*\)\]/\1/')
+echo -e "program interpreter:\n${PROG_INTERPRETER}"
+SHARED_LIB=$(${CROSS_COMPILE}readelf -a ${ROOTFS}/bin/busybox | grep "Shared library")
+echo "Library dependencies:"
+while IFS= read -r line;do
+    echo ${line} | sed 's/.*\[\(.*\)\]/\1/'
+done <<< ${SHARED_LIB}
 
 # TODO: Add library dependencies to rootfs
+if [ -f ${ROOTFS}/lib/${PROG_INTERPRETER} ]; then
+    rm -f ${ROOTFS}/lib/${PROG_INTERPRETER}
+fi
+sudo cp ${INTRPRTR} ${ROOTFS}/lib
+rm -f ${ROOTFS}/lib64/*.*
+sudo cp ${LIBRESOLV} ${ROOTFS}/lib64/
+sudo cp ${LIBC} ${ROOTFS}/lib64/ 
+sudo cp ${LIBM} ${ROOTFS}/lib64/
 
 # TODO: Make device nodes
 
