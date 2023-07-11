c2b7d230ebc2a4a517e277857e50507bae0e0825
Ehsan Azimi <ehs.azimi22@gmail.com>
2023-07-11 17:19:53 +0200

    update the path of conf folder in the target

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,9 +8,9 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
-export PATH="${PATH}:/usr/bin:/etc/finder-app/conf/:/bin"
+# export PATH="${PATH}:/usr/bin:/etc/finder-app/conf/:/bin"
 
 if [ $# -lt 3 ]
 then
@@ -34,7 +34,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
