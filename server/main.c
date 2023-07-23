8fabb7e3de034644b4a4b882993d6d5e05a253b6
Ehsan Azimi <ehs.azimi22@gmail.com>
2023-07-23 13:02:16 +0200

    define a macro to print in the output

--- server/main.c
+++ server/main.c
@@ -12,79 +12,51 @@
 #define RECV_SIZE 1024
 #define FILE_PATH "/var/tmp/aesdsocketdata"
 
+#define CHECK_EXIT_CONDITION(rc, func_name) do { \
+    if((rc) == -1) \
+    { \
+        fprintf(stderr, "%s error: %s", (func_name), strerror(errno)); \
+        exit(EXIT_FAILURE); \
+    } \
+} while(0)
+
 int main(int argc, char **argv)
 {
     openlog("server_log", LOG_PID, LOG_USER);
     int rc_clearfile = clear_file(FILE_PATH);
-    if(rc_clearfile == -1)
-    {
-        fprintf(stderr, "clear_file error: %s", strerror(errno));
-        return -1;
-    }
+    CHECK_EXIT_CONDITION(rc_clearfile, "clear_file");
 
     int sockfd = create_socket();
-    if(sockfd == -1)
-    {
-        fprintf(stderr, "socket error: %s", strerror(errno));
-        return -1;
-    }
+    CHECK_EXIT_CONDITION(sockfd, "create_socket");
 
     char port[5];
     memset(port, 0, sizeof port);
     sprintf(port, "%d", PORT);
     int rc_bind = bind_addr(sockfd, port);
-    if(rc_bind == -1)
-    {
-        fprintf(stderr, "bind_addr error: %s", strerror(errno));
-        return -1;
-    }
+    CHECK_EXIT_CONDITION(rc_bind, "bind_addr");
 
     int rc_listen = listen_conn(sockfd);
-    if(rc_listen == -1)
-    {
-        fprintf(stderr, "listen_conn error: %s", strerror(errno));
-        return -1;
-    }
+    CHECK_EXIT_CONDITION(rc_listen, "listen_conn");
 
     struct sockaddr addr_cli;
     int connfd = accept_conn(sockfd, &addr_cli);
-    if(connfd == -1)
-    {
-        fprintf(stderr, "accept_conn error: %s", strerror(errno));
-        return -1;
-    }
+    CHECK_EXIT_CONDITION(connfd, "accept_conn");
 
     struct sockaddr_in addr;
     int rc_getpeername = getpeername(sockfd, (struct sockaddr *)&addr, (socklen_t *)sizeof(struct sockaddr));
-    if(rc_getpeername == -1)
-    {
-        fprintf(stderr, "getpeername error: %s", strerror(errno));
-        return -1;
-    }
+    CHECK_EXIT_CONDITION(rc_getpeername, "getpeername");
     syslog(LOG_INFO, "Accepted connection from %s", inet_ntoa(addr.sin_addr));
 
     char recv_buff[RECV_SIZE];
     memset((void *)recv_buff, 0, RECV_SIZE);
     int rc_recvdata = recv_data(connfd, recv_buff, RECV_SIZE);
-    if(rc_recvdata == -1)
-    {
-        fprintf(stderr, "recv_data error: %s", strerror(errno));
-        return -1;
-    }
+    CHECK_EXIT_CONDITION(rc_recvdata, "recv_data");
 
     int pfd = open_file(FILE_PATH);
-    if(pfd == -1)
-    {
-        fprintf(stderr, "open_file error: %s", strerror(errno));
-        return -1;
-    }
+    CHECK_EXIT_CONDITION(pfd, "open_file");
 
     int rc_writefile = write_file(pfd, (const void*)recv_buff, strlen(recv_buff));
-    if(rc_writefile == -1)
-    {
-        fprintf(stderr, "write_file error: %s", strerror(errno));
-        return -1;
-    }
+    CHECK_EXIT_CONDITION(rc_writefile, "write_file");
     
     close_socket(sockfd);
     closelog();
